080141bdc69e8706b1742ae912045e16835e47c4
Eren İncedayı <103766421+Erenncday@users.noreply.github.com>
2023-12-20 23:19:31 +0300

    Fluent Validation Paketleri Kuruldu. App User Register için validasyon işlemleri yapıldı.

--- ErenBankMVC/ErenBankMVC.BusinessLayer/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ErenBankMVC.BusinessLayer
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ ErenBankMVC/ErenBankMVC.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,35 @@
+﻿using ErenBankMVC.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErenBankMVC.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Bu Alan Boş Geçilemez!");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Bu alana minimun 2 karakter girilebilir.");
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage("Bu alana maksimum 50 karakter girilebilir.");
+
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Bu Alan Boş Geçilemez!");
+            RuleFor(x => x.Surname).MinimumLength(2).WithMessage("Bu alana minimun 2 karakter girilebilir.");
+            RuleFor(x => x.Surname).MaximumLength(50).WithMessage("Bu alana maksimum 50 karakter girilebilir.");
+
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Bu Alan Boş Geçilemez!");
+            RuleFor(x => x.Username).MinimumLength(2).WithMessage("Bu alana minimun 2 karakter girilebilir.");
+            RuleFor(x => x.Username).MaximumLength(50).WithMessage("Bu alana maksimum 50 karakter girilebilir.");
+
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Bu Alan Boş Geçilemez!");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir mail adresi giriniz.");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Bu Alan Boş Geçilemez!");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Bu Alan Boş Geçilemez!");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Girilen parolalar birbirleriyle eşleşmiyor. Lütfen Tekrar Deneyin!");
+        }
+    }
+}
