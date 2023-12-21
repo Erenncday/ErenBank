e5f14ff2b35516f72288a0b3c73d5e129512efe0
Eren İncedayı <103766421+Erenncday@users.noreply.github.com>
2023-12-21 10:45:03 +0300

    CRUD İşlemleri için gerekli alt yapı hazırlandı.

---
+++ ErenBankMVC/ErenBankMVC.BusinessLayer/Abstract/ICustomerAccountProcessService.cs
@@ -0,0 +1,13 @@
+﻿using ErenBankMVC.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErenBankMVC.BusinessLayer.Abstract
+{
+    public interface ICustomerAccountProcessService : IGenericService<CustomerAccountProcess>
+    {
+    }
+}
---
+++ ErenBankMVC/ErenBankMVC.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,46 @@
+﻿using ErenBankMVC.BusinessLayer.Abstract;
+using ErenBankMVC.DataAccessLayer.Abstract;
+using ErenBankMVC.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErenBankMVC.BusinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : ICustomerAccountProcessService
+    {
+        private readonly ICustomerAccountProcessDAL _customerAccountProcessDAL;
+
+        public CustomerAccountProcessManager(ICustomerAccountProcessDAL customerAccountProcessDAL)
+        {
+            _customerAccountProcessDAL = customerAccountProcessDAL;
+        }
+
+        public void TDelete(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDAL.Delete(t);
+        }
+
+        public CustomerAccountProcess TGetByID(int id)
+        {
+            return _customerAccountProcessDAL.GetByID(id);
+        }
+
+        public List<CustomerAccountProcess> TGetList()
+        {
+            return _customerAccountProcessDAL.GetList();
+        }
+
+        public void TInsert(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDAL.Insert(t);
+        }
+
+        public void TUpdate(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDAL.Update(t);
+        }
+    }
+}
