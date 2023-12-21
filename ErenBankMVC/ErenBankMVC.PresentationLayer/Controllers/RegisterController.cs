99ab907ebe4e2ac450b7f56499cd1a8117b67e88
Eren İncedayı <103766421+Erenncday@users.noreply.github.com>
2023-12-21 15:54:07 +0300

    Register BackEnd tarafı yazıldı

---
+++ ErenBankMVC/ErenBankMVC.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,47 @@
+﻿using ErenBankMVC.DtoLayer.Dtos.AppUserDtos;
+using ErenBankMVC.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErenBankMVC.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        {
+            if(ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName = appUserRegisterDto.Username,
+                    Name = appUserRegisterDto.Name,
+                    Surname = appUserRegisterDto.Surname,
+                    Email = appUserRegisterDto.Email,
+                };
+
+                var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+
+                if (result.Succeeded) 
+                {
+                    return RedirectToAction("Index", "ConfirmMail");
+                }
+            }
+
+            return View();
+        }
+    }
+}
--- ErenBankMVC/ErenBankMVC.PresentationLayer/Program.cs
+++ ErenBankMVC/ErenBankMVC.PresentationLayer/Program.cs
@@ -1,15 +1,22 @@
+using ErenBankMVC.DataAccessLayer.Concrete;
+using ErenBankMVC.EntityLayer.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Context>();
+
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
@@ -17,7 +24,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
